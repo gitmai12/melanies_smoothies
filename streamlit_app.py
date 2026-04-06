8d99e5410c367e3c26b3cd63eeeb56396039673a
gitmai12 <mai12raj@yahoo.com>
2026-04-05 22:47:32 -0500

    Add API call to fetch watermelon data
    Fetch and display watermelon data from Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
