99c97ec63475a23009a69a5a812dc9e9a1a67367
gitmai12 <mai12raj@yahoo.com>
2026-04-05 23:02:09 -0500

    Fix URL formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests  
-smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon)")  https://www.smoothiefroot.com/
+smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon)")
 st.text(smoothiefroot_response.json())
 
 cnx = st.connection("snowflake")
